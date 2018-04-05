6faee1051c1bb493ffaa52bbcf244d33027d8b08
Stefan Oehmcke <stefan.oehmcke@uni-oldenburg.de>
2018-04-05 15:59:25 +0200

    unfolding architecture for easier layer access

--- allconv.py
+++ allconv.py
@@ -1,42 +1,35 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 class AllConvNet(nn.Module):
     def __init__(self, input_size, n_classes=10, **kwargs):
         super(AllConvNet, self).__init__()
-        self.input_drop = nn.Dropout(.2)
-        self.conv_block1 = nn.Sequential(
-            nn.Conv2d(input_size, 96, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(96, 96, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(96, 96, 3, padding=1, stride=2),
-            nn.ReLU(),
-            nn.Dropout(.5),
-        )
-        self.conv_block2 = nn.Sequential(
-            nn.Conv2d(96, 192, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(192, 192, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(192, 192, 3, padding=1, stride=2),
-            nn.ReLU(),
-            nn.Dropout(.5),
-        )
-        self.conv_block3 = nn.Sequential(
-            nn.Conv2d(192, 192, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(192, 192, 1),
-            nn.ReLU(),
-        )
+        self.conv1 = nn.Conv2d(input_size, 96, 3, padding=1)
+        self.conv2 = nn.Conv2d(96, 96, 3, padding=1)
+        self.conv3 = nn.Conv2d(96, 96, 3, padding=1, stride=2)
+        self.conv4 = nn.Conv2d(96, 192, 3, padding=1)
+        self.conv5 = nn.Conv2d(192, 192, 3, padding=1)
+        self.conv6 = nn.Conv2d(192, 192, 3, padding=1, stride=2)
+        self.conv7 = nn.Conv2d(192, 192, 3, padding=1)
+        self.conv8 = nn.Conv2d(192, 192, 1)
+
         self.class_conv = nn.Conv2d(192, n_classes, 1)
         self.global_avg = nn.AdaptiveAvgPool2d(1)
 
 
     def forward(self, x):
-        x = self.input_drop(x)
-        x = self.conv_block1(x)
-        x = self.conv_block2(x)
-        x = self.conv_block3(x)
-        x = self.class_conv(x)
-        x = self.global_avg(x)
-        return x.squeeze()
+        x_drop = F.dropout(x, .2)
+        conv1_out = F.relu(self.conv1(x_drop))
+        conv2_out = F.relu(self.conv1(conv1_out))
+        conv3_out = F.relu(self.conv1(conv2_out))
+        conv3_out_drop = F.dropout(conv3_out, .5)
+        conv4_out = F.relu(self.conv1(conv3_out_drop))
+        conv5_out = F.relu(self.conv1(conv4_out))
+        conv6_out = F.relu(self.conv1(conv5_out))
+        conv6_out_drop = F.dropout(conv6_out, .5)
+        conv7_out = F.relu(self.conv1(conv6_out_drop))
+        conv8_out = F.relu(self.conv1(conv7_out))
+
+        class_out = F.relu(self.class_conv(conv8_out))
+        pool_out = self.adaptive_avg_pool2d(class_out, 1).squeeze()
+        return pool_out
